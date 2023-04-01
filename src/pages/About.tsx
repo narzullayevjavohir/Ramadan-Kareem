c3beb14ff7a80ef52a557a26195c6d0e1863ff5b
Narzullayev Javohir <narzullayevjavohirbek04@gmail.com>
2023-04-01 00:04:16 -0700

    home-page

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import { Box, Progress } from "@chakra-ui/react";
+import axios from "axios";
+
+const About = () => {
+  return (
+    <>
+      <Box w={"100%"} h={"100vh"}></Box>
+    </>
+  );
+};
+
+export default About;
